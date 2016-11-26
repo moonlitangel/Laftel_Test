c3dab0c014052fa26faceb7d411e4eaee31182e0
moonlitangel <thfqlcdl@gmail.com>
2016-11-26 23:08:54 +0900

    Add Likes Message

---
+++ LikesMessage/likesmessage.js
@@ -0,0 +1,18 @@
+function likes(arr) {
+    var strNum = arr.length;
+    if(strNum === 0) return "no one likes this";
+    else if(strNum === 1) return arr + " likes this";
+    else if(strNum === 2) return arr[0] + " and " + arr[1] + " likes this";
+    else if(strNum === 3) return arr[0] + ", " + arr[1] + " and " + arr[2] + " likes this";
+    else if(strNum > 3) return arr[0] + ", " + arr[1] + " and " + (strNum-2) + " others likes this";
+}
+
+var test1 = ["maplestory", "LOL"];
+var test2 = [];
+var test3 = ["maplestory", "LOL", "HOS", "hearthstone"];
+var test4 = ["maplestory", "LOL", "HOS", "hearthstone", "overwatch"];
+
+document.getElementById('test1').innerHTML = '{' + test1 +'} : ' + likes(test1);
+document.getElementById('test2').innerHTML = '{' + test2 +'} : ' + likes(test2);
+document.getElementById('test3').innerHTML = '{' + test3 +'} : ' + likes(test3);
+document.getElementById('test4').innerHTML = '{' + test4 +'} : ' + likes(test4);
