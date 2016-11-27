77c4c357ff2637dedea1fc880f84c4e0f0bac2d6
moonlitangel <thfqlcdl@gmail.com>
2016-11-27 22:53:14 +0900

    Add console log

--- LikesMessage/likesmessage.js
+++ LikesMessage/likesmessage.js
@@ -1,10 +1,14 @@
 function likes(arr) {
+  if(((typeof arr != "undefined") && (typeof arr.valueOf() == "object"))) {
     var strNum = arr.length;
-    if(strNum === 0) return "no one likes this";
-    else if(strNum === 1) return arr + " likes this";
-    else if(strNum === 2) return arr[0] + " and " + arr[1] + " likes this";
-    else if(strNum === 3) return arr[0] + ", " + arr[1] + " and " + arr[2] + " likes this";
-    else if(strNum > 3) return arr[0] + ", " + arr[1] + " and " + (strNum-2) + " others likes this";
+
+    if (strNum === 0) return "no one likes this";
+    else if (strNum === 1) return arr + " likes this";
+    else if (strNum === 2) return arr[0] + " and " + arr[1] + " likes this";
+    else if (strNum === 3) return arr[0] + ", " + arr[1] + " and " + arr[2] + " likes this";
+    else if (strNum > 3) return arr[0] + ", " + arr[1] + " and " + (strNum - 2) + " others likes this";
+  }
+  else return "Not Array"
 }
 
 var test1 = ["maplestory", "LOL"];
@@ -12,7 +16,13 @@ var test2 = [];
 var test3 = ["maplestory", "LOL", "HOS", "hearthstone"];
 var test4 = ["maplestory", "LOL", "HOS", "hearthstone", "overwatch"];
 
-document.getElementById('test1').innerText = '{' + test1 +'} : ' + likes(test1);
-document.getElementById('test2').innerText = '{' + test2 +'} : ' + likes(test2);
-document.getElementById('test3').innerText = '{' + test3 +'} : ' + likes(test3);
-document.getElementById('test4').innerText = '{' + test4 +'} : ' + likes(test4);
+document.getElementById('LM_test1').innerText = '{' + test1 +'} : ' + likes(test1);
+document.getElementById('LM_test2').innerText = '{' + test2 +'} : ' + likes(test2);
+document.getElementById('LM_test3').innerText = '{' + test3 +'} : ' + likes(test3);
+document.getElementById('LM_test4').innerText = '{' + test4 +'} : ' + likes(test4);
+
+console.log("1.Likes Message");
+console.log(likes(test1));
+console.log(likes(test2));
+console.log(likes(test3));
+console.log(likes(test4));
--- ReverseWithTag/reversewithtag.js
+++ ReverseWithTag/reversewithtag.js
@@ -1,28 +1,37 @@
 function reverse(str) {
-  var tag = str.split(" ");
+  if(((typeof str != "undefined") && (typeof str.valueOf() == "string")) && (str.length > 0)) {
+    var tag = str.split(" ");
 
-  for(var i=0;i<tag.length;i++) {
-    if(tag[i].charAt(0) === "<") {
-      var tagName = tag[i].replace("<", "</");
-      for(var j=i+1;j<tag.length;j++) {
-        if(tag[j].match(tagName)) {
-          var temp = [];
-          temp = tag.slice(i, j+1);
-          tag[i] = temp.join(" ");
-          tag.splice(i+1, j-i);
+    for (var i = 0; i < tag.length; i++) {
+      if (tag[i].charAt(0) === "<") {
+        var tagName = tag[i].replace("<", "</");
+        for (var j = i + 1; j < tag.length; j++) {
+          if (tag[j].match(tagName)) {
+            var temp = [];
+            temp = tag.slice(i, j + 1);
+            tag[i] = temp.join(" ");
+            tag.splice(i + 1, j - i);
+          }
         }
       }
     }
+    return tag.reverse().join(" ");
   }
-  return tag.reverse().join(" ");
+  else return "Not String";
 }
 
 var test1 = 'The quick <font color="brown">brown in hi</font> fox jumps over the lazy dog';
 var test2 = 'This is <sub> subscript</sub> and <sup>superscript</sup>';
 var test3 = 'The <font size="2" color="blue">font element</font> is not supported in HTML5. Use CSS instead';
-var test4 = 'The <font size="2" color="blue">font element</font> is <font color="red">not supported</font> in HTML5. Use CSS instead'
+var test4 = 'The <font size="2" color="blue">font element</font> is <font color="red">not supported</font> in HTML5. Use CSS instead';
 
-document.getElementById('test1').innerText = '{' + test1 +'} : ' + reverse(test1);
-document.getElementById('test2').innerText = '{' + test2 +'} : ' + reverse(test2);
-document.getElementById('test3').innerText = '{' + test3 +'} : ' + reverse(test3);
-document.getElementById('test4').innerText = '{' + test4 +'} : ' + reverse(test4);
+document.getElementById('RT_test1').innerText = '{' + test1 +'} :\n' + reverse(test1);
+document.getElementById('RT_test2').innerText = '{' + test2 +'} : \n' + reverse(test2);
+document.getElementById('RT_test3').innerText = '{' + test3 +'} : \n' + reverse(test3);
+document.getElementById('RT_test4').innerText = '{' + test4 +'} : \n' + reverse(test4);
+
+console.log("2. Reverse with Tag");
+console.log(reverse(test1));
+console.log(reverse(test2));
+console.log(reverse(test3));
+console.log(reverse(test4));
